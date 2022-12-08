f68d8ca76a9d4842cc57e634c840038c6204c002
Iván Salazar <ivangio.salazar@gmail.com>
2022-12-08 03:19:23 -0600

    update fonts and colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,39 +6,15 @@ module.exports = {
 	theme: {
 		extend: {
 			colors: {
-				primary: colors.blue,
-				secondary: colors.pink,
+				primary: colors.sky,
+				secondary: colors.lime,
 			},
 			fontFamily: {
-				sans: ["'InterVariable'", ...defaultTheme.fontFamily.sans],
+				sans: ["'Quicksand'", ...defaultTheme.fontFamily.sans],
+				mono: ['courier', ...defaultTheme.fontFamily.mono],
 			},
 		},
 	},
 	plugins: [require('@tailwindcss/typography')],
 	darkMode: 'class',
 };
-
-/* 
-
-  Alternative tailwind.config.js
-  
-  NOTE: Add this fonts to <head>
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;500;700&display=swap" rel="stylesheet" />
-*/
-
-// module.exports = {
-//   content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
-//   theme: {
-//     extend: {
-//       colors: {
-//         primary: colors.cyan,
-//         secondary: colors.lime,
-//       },
-//       fontFamily: {
-//         sans: ["'Nunito'", ...defaultTheme.fontFamily.sans],
-//       },
-//     },
-//   },
-//   plugins: [require("@tailwindcss/typography")],
-//   darkMode: "class",
-// };
